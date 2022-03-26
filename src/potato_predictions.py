ae7589501a0744a56dfd8bb75d526c7b2d5eabec
Asa Paparo <asapaparo@gmail.com>
2022-03-26 12:08:39 -0400

    aaaa

--- src/potato_predictions.py
+++ src/potato_predictions.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from ast import Str
+import string
+from flask import Flask, jsonify, render_template, request
+from itsdangerous import NoneAlgorithm
+from soupsieve import escape
 app = Flask(__name__)
 
-@app.route("/api/<country>")
+@app.route("/api")
 def data(country = "US"):
-    return "a";
+    country = request.args.get("country", "US")
+    day = request.args.get("day", 1)
+    data = {
+        "cases" : "tba"
+    }
+    return jsonify(data);
 
 @app.route("/")
 def hello_world():
-    return 
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
